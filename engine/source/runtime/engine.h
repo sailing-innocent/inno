5eeb7cac4cb74a3bedd5a2da21d08532017bf366
sailing-innocent <sailing-innocent@foxmail.com>
2022-11-06 22:52:57 +0800

    temp add rtow, pending to merge

--- engine/source/runtime/engine.h
+++ engine/source/runtime/engine.h
@@ -8,6 +8,11 @@
 #include <sail/ui/figure.h>
 #include <ing/app/vk_canvas.hpp>
 
+#include <cmath>
+#include <limits>
+#include <memory>
+#include <cstdlib>
+
 namespace inno
 {
 
@@ -17,6 +22,27 @@ int drawRect2D();
 int drawTriangle();
 int drawHistogram(std::vector<float>& data);
 int visualizeDistribution();
+
+const double infinity = std::numeric_limits<double>::infinity();
+const double pi = 3.1415926535897932385;
+
+inline double degrees_to_radians(double degrees) {
+    return degrees * pi / 180.0;
+}
+
+inline double random_double() {
+    return rand() / (RAND_MAX + 1.0);
+}
+
+inline double random_double(double _min, double _max) {
+    return _min + (_max - _min) * random_double();
+}
+
+inline double clamp(double x, double _min, double _max) {
+    if (x < _min) { return _min; }
+    if (x > _max) { return _max; }
+    return x;
+}
 
 class InnoEngine
 {
---
+++ engine/source/runtime/function/ray/sphere.h
@@ -0,0 +1,58 @@
+#pragma once
+
+#ifndef SPHERE_H
+#define SPHERE_H
+
+#include "runtime/function/ray/hittable.h"
+#include "runtime/core/math/vec3.h"
+
+namespace inno {
+
+class sphere: public hittable {
+public:
+    sphere() {}
+    sphere(point3 cen, double r, std::shared_ptr<material> m): center(cen), radius(r), mat_ptr(m){};
+    virtual bool hit(
+        const ray& r, double t_min, double t_max, hit_record& rec
+    ) const override;
+public:
+    point3 center;
+    double radius;
+    std::shared_ptr<material> mat_ptr;
+};
+
+bool sphere::hit(const ray& r, double t_min, double t_max, hit_record& rec) const
+{
+    vec3 oc = r.origin() - center;
+    auto a = r.direction().length_squared();
+    auto half_b = dot(oc, r.direction());
+    auto c = oc.length_squared() - radius * radius;
+
+    auto discriminant = half_b * half_b - a * c;
+    if (discriminant < 0.0) {
+        return false;
+    } 
+    auto sqrtd = sqrt(discriminant);
+
+    // Find the nearest root that lies in the acceptable range.
+
+    auto root = (-half_b - sqrtd) / a;
+    if (root < t_min || t_max < root) {
+        root = (-half_b + sqrtd) / a;
+        if (root < t_min || t_max < root)
+            return false;
+    }
+
+    rec.t = root;
+    rec.p = r.at(rec.t);
+
+    vec3 outward_normal = (rec.p - center) / radius;
+    rec.set_face_normal(r, outward_normal);
+    rec.mat_ptr = mat_ptr;
+
+    return true;
+}
+
+} // namespace inno
+
+#endif
