dd2b0d379cb0a15c450b53dd87e197a0a69acfc6
sailing-innocent <sailing-innocent@foxmail.com>
2022-11-08 00:11:42 +0800

    add basic vert frag

---
+++ engine/source/common.h
@@ -0,0 +1,20 @@
+#pragma once
+/**
+ * @file: inno/engine/source/common.h
+ * @author: sailing-innocent
+ * @create: 2022-11-07
+ * @desp: The Common Header for INNO Engine
+*/
+
+#ifndef INNO_COMMON_H_
+#define INNO_COMMON_H_
+
+#define INNO_NAMESPACE_BEGIN namespace inno {
+#define INNO_NAMESPACE_END }
+
+INNO_NAMESPACE_BEGIN
+
+INNO_NAMESPACE_END
+
+#endif // INNO_COMMON_H_
+
