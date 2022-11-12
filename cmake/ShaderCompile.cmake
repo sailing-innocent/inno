bb8bba8cf6486d39df84c23681f398f531df9d8c
sailing-innocent <sailing-innocent@foxmail.com>
2022-11-12 22:08:48 +0800

    feat: add shader compile support

--- cmake/ShaderCompile.cmake
+++ cmake/ShaderCompile.cmake
@@ -1,10 +1,16 @@
 function(compile_shader SHADERS TARGET_NAME SHADER_INCLUDE_FOLDER GENERATED_DIR GLSLANG_BIN)
+
     set(working_dir "${CMAKE_CURRENT_SOURCE_DIR}")
 
     set(ALL_GENERATED_SPV_FILES "")
     set(ALL_GENERATED_CPP_FILES "")
 
+    if(UNIX)
+        execute_process(COMMAND chmod a+x ${GLSLANG_BIN})
+    endif()
+
     foreach(SHADER ${SHADERS})
+    # Prepare a header name and a global variable for this shader
         get_filename_component(SHADER_NAME ${SHADER} NAME)
         string(REPLACE "." "_" HEADER_NAME ${SHADER_NAME})
         string(TOUPPER ${HEADER_NAME} GLOBAL_SHADER_VAR)
@@ -16,20 +22,23 @@ function(compile_shader SHADERS TARGET_NAME SHADER_INCLUDE_FOLDER GENERATED_DIR 
             OUTPUT ${SPV_FILE}
             COMMAND ${GLSLANG_BIN} -I${SHADER_INCLUDE_FOLDER} -V100 -o ${SPV_FILE} ${SHADER}
             DEPENDS ${SHADER}
-            WORKING_DIRECTORY "${working_dir}"
-        )
+            WORKING_DIRECTORY "${working_dir}")
 
         list(APPEND ALL_GENERATED_SPV_FILES ${SPV_FILE})
+
+        message("${CMAKE_COMMAND} -DPATH=${SPV_FILE} -DHEADER=\"${CPP_FILE}\" -DGLOBAL=\"${GLOBAL_SHADER_VAR}\" -P \"${INNO_ROOT_DIR}/cmake/GenerateShaderCPPFile.cmake\"}")
         add_custom_command(
             OUTPUT ${CPP_FILE}
-            COMMAND ${CMAKE_COMMAND} -DPATH=${SPV_FILE} -DHEADER="${CPP_FILE}"
+            COMMAND ${CMAKE_COMMAND} -DPATH=${SPV_FILE} -DHEADER="${CPP_FILE}" 
                 -DGLOBAL="${GLOBAL_SHADER_VAR}" -P "${INNO_ROOT_DIR}/cmake/GenerateShaderCPPFile.cmake"
             DEPENDS ${SPV_FILE}
-            WORKING_DIRECTORY "${working_dir}"
-        )
+            WORKING_DIRECTORY "${working_dir}")
+
         list(APPEND ALL_GENERATED_CPP_FILES ${CPP_FILE})
+
     endforeach()
 
     add_custom_target(${TARGET_NAME}
         DEPENDS ${ALL_GENERATED_SPV_FILES} ${ALL_GENERATED_CPP_FILES} SOURCES ${SHADERS})
+
 endfunction()
--- engine/shader/glsl/axis.frag
+++ engine/shader/glsl/axis.frag
@@ -1,9 +1,10 @@
 #version 460
 
 layout (location = 0) in vec3 in_color;
+
 layout (location = 0) out vec4 outColor;
 
-void main()
+void main() 
 {
-    outColor = vec4(in_color, 1.0);
+	outColor = vec4(in_color, 1.0);
 }
